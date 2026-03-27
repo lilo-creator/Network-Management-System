32eb39b1482bc83ebde8a6f23e9f7e92a97ad6dd
lilian <lmlilian702@gmail.com>
2026-03-27 19:43:26 +0300

    Add DisconnectLog model and run migrations

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class DisconnectLog(models.Model):
+    admin_username = models.CharField(max_length=100)
+    user_ip = models.GenericIPAddressField()
+    user_mac = models.CharField(max_length=17)
+    user_hostname = models.CharField(max_length=100, blank=True)
+    disconnected_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.admin_username} disconnected {self.user_ip} at {self.disconnected_at}"
+
+    class Meta:
+        ordering = ['-disconnected_at']
